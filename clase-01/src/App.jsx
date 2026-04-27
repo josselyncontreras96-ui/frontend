58511a037b6e7fde8735082933a5713b16c5e8a2
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-04-27 18:35:00 +0200

    Component App

---
+++ clase-01/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App () {
+const name = "Josselyn Contreras";
+const age = 30;
+
+  return (
+    <>          
+      <h1>Hola {name} </h1>
+      <p>Bienvenida a React</p>
+      <h2>tengo {age} años</h2>
+    </>
+    
+  );
+}
+
+export default App
