d638b5835e15e787d39c5a900ccb232344be0083
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-05-18 18:43:17 +0200

    loaders

---
+++ clase-rutas/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function Layout() {
+  return (
+    <main className="container">
+      <h1>Clase rutas</h1>
+
+      <Navbar />
+
+      <Outlet />
+    </main>
+  );
+}
+
+export default Layout;
